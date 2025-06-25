0b8253c317b370a9e62d24b00d8a6bc37d6b0028
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-06-25 11:26:41 +0100

    refactor sampler

--- finetune.py
+++ finetune.py
@@ -1,26 +1,31 @@
-# simulate 8 devices
+# simulate multiple devices
 import os
-os.environ['XLA_FLAGS'] = '--xla_force_host_platform_device_count=4'
+os.environ['XLA_FLAGS'] = '--xla_force_host_platform_device_count=1'
 
 import jax
 import timing
-from gemma import sampler as gemma_sampler
-from gemma import pretrained as gemma_pretrained
-
+import numpy as np
+import jax.numpy as jnp
+from flax import nnx
+import gemma
+import data
+from sampler import sample
 
 # config
-model_variant = 'gemma3-4b-it-int4'
+model_variant = 'gemma3-1b-it-int4'
 mesh = jax.make_mesh((1, jax.device_count()), ('data', 'model'))
 
 with timing.context('load model'):    
-    model, vocab = gemma_pretrained.load_pretrained(model_variant, mesh)
+    model, vocab = gemma.load_pretrained(model_variant, mesh)
     # jax.debug.visualize_array_sharding(model.embedder.input_embedding.value)
 
 # sampling
 with timing.context('sampling'):
     with mesh:
-        sampler = gemma_sampler.Sampler(model, vocab, max_seq_len=100)
-        input_batch = ["Say 'Hi!':", "Write a Python program that prints 'Hello World!':"]
-        output_batch = sampler(input_batch)
-        for inpt, output in zip(input_batch, output_batch):
+        inputs_text = ["Say 'Hi!':", "Write a Python program that prints 'Hello World!':"]
+        inputs_tokenized = jnp.array(data.tokenize(inputs_text, vocab, 100))
+        eos_ids = jnp.array([vocab.eos_id(), vocab.EncodeAsIds('<end_of_turn>')[0]])
+        outputs_tokens = sample(model, inputs_tokenized, eos_ids)
+        outputs_text = vocab.DecodeIds(outputs_tokens)
+        for inpt, output in zip(inputs_text, outputs_text):
             print(f"Prompt:\n{inpt}\nOutput:\n{output}\n{10*'*'}")
