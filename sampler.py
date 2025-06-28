fix length computation

sampler.py
@@ -54,6 +54,8 @@ def sample(model, tokens, pad_id=0, eot_id=106):
     # extract output sequences
     outputs = []
     for in_seq, out_seq in zip(tokens, state.tokens):
-        outputs += [out_seq[jnp.argmax(in_seq==pad_id):].tolist()]
+        out_seq = out_seq[jnp.argmax(in_seq==pad_id):]
+        if jnp.any(out_seq==pad_id): out_seq = out_seq[:jnp.argmax(out_seq==pad_id)]
+        outputs += [out_seq.tolist()]
 
     return outputs